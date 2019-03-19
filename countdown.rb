ed3b56725e816bafa671d06a83cfe3768959492e
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 00:54:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,16 @@
 def countdown(Integer)
   while Integer > 0 do
     puts "#{number} SECOND(S)!"
+    Integer -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(Integer)
+  while Integer > 0 do
+    puts "#{number} SECOND(S)!"
+    Integer -= 1
+    
   end
   puts "HAPPY NEW YEAR!"
 end
